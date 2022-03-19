8dcf1f0a27e6623fd7727337efda69231e42e208
HangyuanXu <54507204+HangyuanXu@users.noreply.github.com>
2022-03-19 02:48:19 -0700

    update

--- web/src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from 'C:/Users/xu hangyuan/Desktop/273lab1/web/node_modules/react-helmet';
--- web/src/services/api.js
+++
@@ -1,3 +0,0 @@
-export default {
-    loginUser: 'POST /user/login',
-}
